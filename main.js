5cdbffb35b599e78da46890da7f5b7acc9def645
hwasser <victorhwasser@gmail.com>
2021-01-27 12:30:57 +0100

    New basic design

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#login_button").click(function() {
+        $("#login").css("display", "none");
+        $(".main").css("display", "block");
+        // TODO: different logins for different user types ...
+    });
+});
+
