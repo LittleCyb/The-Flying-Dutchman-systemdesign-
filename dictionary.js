225f1dc7a2ebe89a91eaea241ec1160ce7976c30
Gideon <>
2021-02-08 15:43:56 +0100

    rebased onto changes from main

--- dictionary.js
+++ dictionary.js
@@ -1,35 +1,38 @@
 /*
  File: dictionary.js
-
  Author: TODO: add names
-
-
  this js document contains the language data and related functions for the application
  */
 
-var language = 'en'
+var language = 'sv'
 
 dict = {
     'keys': ['login_text',
             'login_button',
             'menu_bar_beers',
             'menu_bar_spirits',
-            'footer'],
+            'login_manager'],
+    'pics': ['language',
+            'logo'],
 
     'en' : {
+        'language' : "sv.jpg",
+        'logo' : "logo.png",
         'login_text': "Login as admin, enter password below",
         'login_button': "Login",
         'menu_bar_beers': "Beer",
         'menu_bar_spirits': "Spirits",
-        'footer' : "Design by Shores of Silence"
+        'login_manager': "Login as manager"
     },
 
     'sv' : {
+        'language': "eng.jpg",
+        'logo' : "logo.png",
         'login_text': "Logga in som admin, ange lösenord nedan",
         'login_button': "Logga in",
         'menu_bar_beers': "Öl",
         'menu_bar_spirits': "Sprit",
-        'footer' : "Design by Shores of Silence"
+        'login_manager': "Logga in som manager"
     }
 }
 
@@ -41,7 +44,7 @@ function change_language() {
     if (language =='en') {
         language = 'sv';
     } else { language = 'en'};
-    update_viev();
+    update_view();
 }
 
 /* END OF FILE */
--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 /*
  File: main.js
-
  Author: TODO: add names
-
-
  this js document contains the control logic for the application
  */
+
+
+/* language selector */
+$('body').append('<div id="language_bar"</div>');
+$("#language_bar").append('<img id="language" src="">');
 
 /* login admin screen */
 
 $('body').append('<div id="login"></div>');
 
-$("#login").append('<img id="logo" src="logo.png">');
+$("#login").append('<div id="login_topbar"></div>');
+$("#login_topbar").append('<span id="login_manager"></span>');
+
+$("#login").append('<img id="logo" src="">');
 
 $("#login").append('<p id="login_text"></p>');
 
@@ -23,7 +28,7 @@ $("#login").append('<span id="login_button"></span>');
 // menu
 
 
-$('body').append('<div id="menu"></div>')
+$('body').append('<div id="menu"></div>');
 
 $("#menu").append('<img id="logo" src="logo.png">');
 
@@ -37,29 +42,29 @@ $("#menu").append('<div id="menu_view"></div>');
 
 $("#menu_view").append('<p> Lorem ipsum doltie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commod </p>');
 
+//manager
+$('body').append('<div id="manager"></div>');
+
+$("menu").append('<p>Manager Site</p>');
+
 
 /*
-
 // CHEAT: get random items in menu and in customer order
 for (var i = 0; i < 4; i++) {
 	var item = $('<div class="menu_item"></div>');
 	$(item).attr("id", "item_" + i);
 	$(item).text("beer " + (i+1));
 	$("#menu").append(item);
-
 	var item_order = $('<div class="order_item"></div>');
 	$(item_order).attr("id", "item_order_" + i);
 	$(item_order).text("beer " + (i+1) + " x1");
 	$("#order").append(item_order);
 }
-
 // Checkout
 $("#guest").append('<div id="checkout"></div>');
-
 var total_cost = $('<p id="total_cost"></p>')
 $(total_cost).text("Total: " + get_total());
 $("#checkout").append(total_cost);
-
 // Return total cost of order
 function get_total() {
 	return 100; // CHEAT
@@ -69,6 +74,13 @@ function get_total() {
 function login_menu_login() {
 	$("#login").css("display", "none");
 	$("#menu").css("display", "block");
+	update_view();
+}
+
+function login_manager() {
+	$("#login").css("display", "none");
+	$("#manager").css("display", "block");
+	update_view();
 }
 
 //updates view with text in Swedish or English
@@ -77,6 +89,11 @@ function update_view() {
 	for (idx in keys) {
 		key = keys[idx];
 		$("#" + key).text(get_string(key));
+	}
+	pics = dict['pics'];
+	for (idx in pics) {
+		pic = pics[idx];
+		$("#" + pic).attr('src', get_string(pic));
 	}
 }
 
@@ -99,8 +116,11 @@ function update_view() {
 
 $(document).ready(function() {
 	update_view();
-	$("#login").css("display", "block"); //change back to #login later
+	$("#language_bar").css("display", "flex");
+	$("#login").css("display", "block");
+	document.getElementById("language").addEventListener("click", change_language);
 	document.getElementById("login_button").addEventListener("click", login_menu_login);
+	document.getElementById("login_manager").addEventListener("click", login_manager);
 	}
 );
 
