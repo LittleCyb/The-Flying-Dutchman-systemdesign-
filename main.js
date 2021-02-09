921d8c50a2e8f2c01e219b98d7435e37118abf13
Gideon <>
2021-02-09 13:04:49 +0100

    works with login->choose/manager->menu screens, removes old frames

--- main.js
+++ main.js
@@ -5,87 +5,117 @@
  */
 
 
-/* language selector */
-$('body').append('<div id="language_bar"</div>');
-$("#language_bar").append('<img id="language" src="">');
+ /**
+  * load_topbar_language
+  * @desc Creates a language topbar
+  */
+function load_topbar_language() {
+	$('body').append('<div id="language_bar"</div>');
+	$("#language_bar").append('<img id="language" src="">');
+	document.getElementById("language").addEventListener("click", change_language);
+}
 
-/* login admin screen */
+/**
+ * load_frame_login
+ * @desc Creates a login frame
+ * @param old_frame Old frame to be removed
+ */
+function load_frame_login(old_frame) {
+	// Removes the old frame
+	if (old_frame) {
+		$("#" + old_frame).remove();
+	}
+	// Adds the new frame
+	$('body').append('<div id="login"></div>');
+	$("#login").append('<div id="login_topbar"></div>');
+	$("#login_topbar").append('<span id="login_manager"></span>');
+	$("#login_manager").attr("onclick", 'load_frame_manager("login")');
 
-$('body').append('<div id="login"></div>');
+	$("#login").append('<img id="logo" src="">');
 
-$("#login").append('<div id="login_topbar"></div>');
-$("#login_topbar").append('<span id="login_manager"></span>');
+	$("#login").append('<p id="login_text"></p>');
 
-$("#login").append('<img id="logo" src="">');
+	$("#login").append('<span id="login_input"></span>');
+	$("#login_input").attr("contentEditable", "true");
 
-$("#login").append('<p id="login_text"></p>');
+	$("#login").append('<span id="login_button"></span>');
+	$("#login_button").attr("onclick", 'load_frame_choose("login")');
+	update_view();
+}
 
-$("#login").append('<span id="login_input"></span>');
-$("#login_input").attr("contentEditable", "true");
+/**
+ * load_frame_choose
+ * @desc Creates a choose table frame
+ * @param old_frame Old frame to be removed
+ */
+function load_frame_choose(old_frame) {
+	// Removes the old frame
+	if (old_frame) {
+		$("#" + old_frame).remove();
+	}
+	// Adds the new frame
+	$('body').append('<div id="choose_screen"></div>');
+	$("#choose_screen").append('<img id="logo" src="">');
+	// Add tables
+	for (i = 1; i <= 9; i++) {
+		var table = $('<div class="table"></div>');
+		$(table).attr("id", "table_" + i);
+		$(table).attr("onclick", 'load_frame_menu("choose_screen")');
+		$(table).text("Table " + i)
+		$("#choose_screen").append(table);
+	}
+	// Add bar
+	$('#choose_screen').append('<div class="table" id="table_bar"> Bar </div>');
+	update_view();
+}
 
-$("#login").append('<span id="login_button"></span>');
+/**
+ * load_frame_login
+ * @desc Creates a menu frame
+ * @param old_frame Old frame to be removed
+ */
+function load_frame_menu(old_frame) {
+	// Removes the old frame
+	if (old_frame) {
+		$("#" + old_frame).remove();
+	}
+	// Adds the new frame
+	$('body').append('<div id="menu"></div>');
 
-// menu
+	$("#menu").append('<div id="menu_topbar"></div>');
+	$("#menu_topbar").append('<span id="login_vip"></span>');
 
+	$("#menu").append('<img id="logo" src="logo.png">');
 
-$('body').append('<div id="menu"></div>');
+	$("#menu").append('<div id="menu_bar"></div>');
 
-$("#menu").append('<div id="menu_topbar"></div>');
-$("#menu_topbar").append('<span id="login_vip"></span>');
+	$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_beers"></div>');
 
-$("#menu").append('<img id="logo" src="logo.png">');
+	$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_cocktails"></div>');
 
-$("#menu").append('<div id="menu_bar"></div>');
+	$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_wine"></div>');
 
-$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_beers"></div>');
+	$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_vip"></div>');
 
-$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_cocktails"></div>');
+	$("#menu").append('<div id="menu_view"></div>');
 
-$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_wine"></div>');
+	$("#menu_view").append('<p> Lorem ipsum doltie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commod </p>');
 
-$("#menu_bar").append('<div class="menu_bar_item" id="menu_bar_vip"></div>');
+	update_view();
+}
 
-$("#menu").append('<div id="menu_view"></div>');
+/**
+ * load_frame_manager
+ * @desc Creates a manager frame
+ * @param old_frame Old frame to be removed
+ */
+function load_frame_manager(old_frame) {
+	// Removes the old frame
+	if (old_frame) {
+		$("#" + old_frame).remove();
+	}
 
-$("#menu_view").append('<p> Lorem ipsum doltie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commodtie, mollis ut elit. Aenean arcu velit, congue non rhoncus ac, malesuada bibendum ipsum. Sed pretium imperdiet mattis. Phasellus sed felis erat. Phasellus nec neque felis. Morbi nec ante libero. Nullam eget tellus consectetur, elementum purus sit amet, efficitur nunc. Sed mattis nisi at dolor cursus, vel dignissim neque cursus. Vestibulum elementum lacinia quam, in elementum nunc consectetur consectetur. Phasellus sollicitudin ipsum metus, vitae faucibus nulla venenatis non. Fusce quis nisi lacus. Integer semper diam nec libero blandit, a finibus felis maximus. Nulla interdum commod  </p>');
-
-//manager
 $('body').append('<div id="manager"></div>');
-
-
-/*
-// CHEAT: get random items in menu and in customer order
-for (var i = 0; i < 4; i++) {
-	var item = $('<div class="menu_item"></div>');
-	$(item).attr("id", "item_" + i);
-	$(item).text("beer " + (i+1));
-	$("#menu").append(item);
-	var item_order = $('<div class="order_item"></div>');
-	$(item_order).attr("id", "item_order_" + i);
-	$(item_order).text("beer " + (i+1) + " x1");
-	$("#order").append(item_order);
-}
-// Checkout
-$("#guest").append('<div id="checkout"></div>');
-var total_cost = $('<p id="total_cost"></p>')
-$(total_cost).text("Total: " + get_total());
-$("#checkout").append(total_cost);
-// Return total cost of order
-function get_total() {
-	return 100; // CHEAT
-}
-*/
-
-function login_menu_login() {
-	$("#login").css("display", "none");
-	$("#menu").css("display", "block");
-	update_view();
-}
-
-function login_manager() {
-	$("#login").css("display", "none");
-	$("#manager").css("display", "block");
-	update_view();
 }
 
 //updates view with text in Swedish or English
@@ -120,14 +150,9 @@ function update_view() {
 //
 
 $(document).ready(function() {
-	update_view();
-	$("#language_bar").css("display", "flex");
-	$("#login").css("display", "block");
-	document.getElementById("language").addEventListener("click", change_language);
-	document.getElementById("login_button").addEventListener("click", login_menu_login);
-	document.getElementById("login_manager").addEventListener("click", login_manager);
-	}
-);
+	load_topbar_language();
+	load_frame_login();
+});
 
 
 // ===========================================================================
